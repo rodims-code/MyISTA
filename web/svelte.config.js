829f60c32e18852bdcf7f7f16b25ec926a05b75d
rodims-code <ridrodims@hotmail.com>
2026-03-29 15:39:10 +0100

    fixing a11y

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -2,6 +2,11 @@ import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	compilerOptions: {
+		a11y: {
+			warnOnly: true
+		}
+	},
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
