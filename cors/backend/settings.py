570ac964ae5afecf237e372d543af379d0655571
rodims-code <ridrodims@hotmail.com>
2026-03-30 08:48:31 +0100

    add static root in the settings django

--- cors/backend/settings.py
+++ cors/backend/settings.py
@@ -160,7 +160,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static files
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
