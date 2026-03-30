2a60cdcc767091ccf9946b6415e884e4ec066852
rodims-code <ridrodims@hotmail.com>
2026-03-30 08:55:38 +0100

    update static_root

--- cors/backend/settings.py
+++ cors/backend/settings.py
@@ -163,7 +163,7 @@ USE_TZ = True
 # Static files
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
