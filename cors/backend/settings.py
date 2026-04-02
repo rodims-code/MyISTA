5ecfb64b6fe1a60c9dfc21fe34627d7241aa8e83
rodims-code <ridrodims@hotmail.com>
2026-04-02 01:39:32 +0100

    change the db settings

--- cors/backend/settings.py
+++ cors/backend/settings.py
@@ -134,19 +134,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-if os.environ.get('ENV') == 'production':
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=os.environ.get('DATABASE_URL')
-        )
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
+}
 
 
 # Password validation
