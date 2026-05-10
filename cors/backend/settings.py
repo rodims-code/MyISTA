ef93240277822503c5a1f40a16f1e2286271b7fd
RODIM'S Dieuveil <ridrodims@hotmail.com>
2026-05-10 13:04:01 +0100

    Update settings.py

--- cors/backend/settings.py
+++ cors/backend/settings.py
@@ -200,8 +200,11 @@ STORAGES = {
 CORS_ALLOWED_ORIGINS = [
     "https://my-ista-seven.vercel.app",
     "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "https://myista-production.up.railway.app",
+    "http://127.0.0.1:5173",      "https://myista-production.up.railway.app",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+"https://myista-production.up.railway.app",
 ]
 
 from corsheaders.defaults import default_headers
